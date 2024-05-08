7978ff67e618c172afd5158aabe875a1446908a3
Pavel Klimenko <p.klimenko@itachsoft.com>
2024-05-08 19:27:40 +0300

    init commit

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,20 +1,32 @@
 <template>
-    <div class="wrapper">
-<!--        <header></header>-->
-        <div class="wrapper-content">
-            <section>
-                <div class="container">
-                    <h1>Hello</h1>
-                    <img alt="Vue logo" src="./assets/logo.png">
-                </div>
-            </section>
-        </div>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hello World, meet Buddy!"/>
+    <br />    <br />    <br />
+
+    <HelloWorld1 msg1="Hello World, meet Buddy!44444"/>
+  </div>
 </template>
 
 <script>
-export default {}
+import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld1 from './components/HelloWorld1.vue'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    HelloWorld1
+  }
+}
 </script>
 
-<style lang="scss">
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- app/src/components/HelloWorld1.vue
+++ app/src/components/HelloWorld1.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg1 }}</h1>
     <p>
       For more info on how Buddy can help you launch killer apps,<br>
       check out the
       <a href="http://buddy.works" target="_blank" rel="noopener">Buddy homepage</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Installed CLI Plugins1</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3>Ecosystem1</h3>
     <ul>
       <li><a href="https://buddy.works/pricing" target="_blank" rel="noopener">Pricing</a></li>
       <li><a href="https://buddy.works/knowledge" target="_blank" rel="noopener">Docs</a></li>
@@ -19,7 +19,7 @@
       <li><a href="https://app.buddy.works/sign-up" target="_blank" rel="noopener">Free Trial</a></li>
       <li><a href="https://buddy.works/blog" target="_blank" rel="noopener">Blog</a></li>
     </ul>
-    <h3>Social Media</h3>
+    <h3>Social Media1</h3>
     <ul>
       <li><a href="https://www.facebook.com/gitbuddy" target="_blank" rel="noopener">Facebook</a></li>
       <li><a href="https://twitter.com/buddygit" target="_blank" rel="noopener">Twitter</a></li>
@@ -32,9 +32,9 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'HelloWorld1',
   props: {
-    msg: String
+    msg1: String
   }
 }
 </script>
